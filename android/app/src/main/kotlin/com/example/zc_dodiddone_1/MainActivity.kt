248e708d3e0d4f3c2ff2c05514d99ba025ec1086
Alinash2024 <interspeis@gmail.com>
2024-07-26 15:40:35 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/zc_dodiddone_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.zc_dodiddone_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:zc_dodiddone_1/app/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
